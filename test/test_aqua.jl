b2cd70c595124ed40cc51170050450abf697a6d7
mtfishman <mfishman@flatironinstitute.org>
2024-11-26 16:06:10 -0500

    Package setup

---
+++ test/test_aqua.jl
@@ -0,0 +1,9 @@
+@eval module $(gensym())
+using SymmetrySectors: SymmetrySectors
+using Aqua: Aqua
+using Test: @testset
+
+@testset "Code quality (Aqua.jl)" begin
+  Aqua.test_all(SymmetrySectors)
+end
+end
